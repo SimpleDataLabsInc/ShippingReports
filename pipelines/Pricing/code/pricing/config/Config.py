ff5bff85f296570a1e4f22c97c572e595e792a95
anya.bida1@gmail.com <anya.bida1@gmail.com>
2023-03-02 21:59:16 +0000

    add path config for targets

--- pipelines/Pricing/code/pricing/config/Config.py
+++ pipelines/Pricing/code/pricing/config/Config.py
@@ -4,14 +4,21 @@ prophecy_spark_context = None
 
 class Config(ConfigBase):
 
-    def __init__(self, segment: str=None, datestring: str=None, Database: str=None):
+    def __init__(self, segment: str=None, datestring: str=None, Database: str=None, path_helper: str=None):
         self.spark = None
-        self.update(segment, datestring, Database)
+        self.update(segment, datestring, Database, path_helper)
 
-    def update(self, segment: str="BUILDING", datestring: str="1993-11-13", Database: str="RETAIL"):
+    def update(
+            self,
+            segment: str="BUILDING", 
+            datestring: str="1993-11-13", 
+            Database: str="RETAIL", 
+            path_helper: str="Path1"
+    ):
         global prophecy_spark_context
         prophecy_spark_context = self.spark
         self.segment = segment
         self.datestring = datestring
         self.Database = Database
+        self.path_helper = path_helper
         pass
--- pipelines/Pricing/code/pricing/graph/UnshippedOrders.py
+++ pipelines/Pricing/code/pricing/graph/UnshippedOrders.py
@@ -7,4 +7,7 @@ from pricing.udfs.UDFs import *
 
 def UnshippedOrders(spark: SparkSession, in0: DataFrame):
     from delta.tables import DeltaTable, DeltaMergeBuilder
-    in0.write.format("delta").mode("overwrite").save("dbfs:/Prophecy/sparklearner123@gmail.com/Pricing/UnshippedOrders")
+    in0.write\
+        .format("delta")\
+        .mode("overwrite")\
+        .save(f"dbfs:/Prophecy/sparklearner123@gmail.com/Pricing/{Config.path_helper}/UnshippedOrders")
