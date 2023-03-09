c30a646a99a32c8e2af74c357e8e4eb04a557d02
anya.bida1@gmail.com <anya.bida1@gmail.com>
2023-03-09 22:36:23 +0000

    added configurable environment variable, and configurable encryption logic

--- pipelines/Pricing/code/pricing/config/Config.py
+++ pipelines/Pricing/code/pricing/config/Config.py
@@ -4,11 +4,13 @@ prophecy_spark_context = None
 
 class Config(ConfigBase):
 
-    def __init__(self, ):
+    def __init__(self, env: str=None, encrypt_logic: str=None):
         self.spark = None
-        self.update()
+        self.update(env, encrypt_logic)
 
-    def update(self, ):
+    def update(self, env: str="dev", encrypt_logic: str="aes_encrypt(COMMENT, secret('dev-scope', 'AESkey'))"):
         global prophecy_spark_context
         prophecy_spark_context = self.spark
+        self.env = env
+        self.encrypt_logic = encrypt_logic
         pass
