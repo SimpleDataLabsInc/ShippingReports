0ed8a6bd548da8624f1fbb9e2fa3a7c96749992f
anya.bida1@gmail.com <anya.bida1@gmail.com>
2023-03-10 21:56:17 +0000

    blankest slate

--- pipelines/Pricing/code/pricing/config/Config.py
+++ pipelines/Pricing/code/pricing/config/Config.py
@@ -4,13 +4,11 @@ prophecy_spark_context = None
 
 class Config(ConfigBase):
 
-    def __init__(self, env: str=None, encryption_logic: str=None):
+    def __init__(self, ):
         self.spark = None
-        self.update(env, encryption_logic)
+        self.update()
 
-    def update(self, env: str="dev", encryption_logic: str="aes_encrypt(COMMENT, secret('dev-scope', 'AESkey'))"):
+    def update(self, ):
         global prophecy_spark_context
         prophecy_spark_context = self.spark
-        self.env = env
-        self.encryption_logic = encryption_logic
         pass
--- pipelines/Pricing/code/pricing/graph/Shipments.py
+++ pipelines/Pricing/code/pricing/graph/Shipments.py
@@ -15,7 +15,7 @@ def Shipments(spark: SparkSession) -> DataFrame:
             "sfUrl": "https://lzcxmtj-mn69040.snowflakecomputing.com",
             "sfUser": DBUtils(spark).secrets.get(scope = "anyademos", key = "username"),
             "sfPassword": DBUtils(spark).secrets.get(scope = "anyademos", key = "password"),
-            "sfDatabase": f"{Config.env}_retail",
+            "sfDatabase": "retail",
             "sfSchema": "PRICING",
             "sfWarehouse": "TPC"
           }
