d7224dc89f462a37f645e03489f42e456d8b21bb
anya.bida1@gmail.com <anya.bida1@gmail.com>
2023-03-23 19:25:22 +0000

    adjust tax in demo

--- pipelines/PricingBySegment/code/pricingbysegment/graph/Pricing.py
+++ pipelines/PricingBySegment/code/pricingbysegment/graph/Pricing.py
@@ -7,7 +7,4 @@ from pricingbysegment.udfs.UDFs import *
 
 def Pricing(spark: SparkSession, in0: DataFrame):
     from delta.tables import DeltaTable, DeltaMergeBuilder
-    in0.write\
-        .format("delta")\
-        .mode("overwrite")\
-        .save("dbfs:/Prophecy/sparklearner123@gmail.com/RetailVendor/default/PricingReport")
+    in0.write.format("delta").mode("overwrite").save("dbfs:/Prophecy/sparklearner123@gmail.com/RetailVendor/MACHINERY/")
