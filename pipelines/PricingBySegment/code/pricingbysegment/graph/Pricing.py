6f8c4b0e6a0db744b10aedfa9bfe8effee18a4a9
anya.bida1@gmail.com <anya.bida1@gmail.com>
2023-03-23 19:32:01 +0000

    clean slate for retake

--- pipelines/PricingBySegment/code/pricingbysegment/graph/Pricing.py
+++ pipelines/PricingBySegment/code/pricingbysegment/graph/Pricing.py
@@ -7,4 +7,7 @@ from pricingbysegment.udfs.UDFs import *
 
 def Pricing(spark: SparkSession, in0: DataFrame):
     from delta.tables import DeltaTable, DeltaMergeBuilder
-    in0.write.format("delta").mode("overwrite").save("dbfs:/Prophecy/sparklearner123@gmail.com/RetailVendor/MACHINERY/")
+    in0.write\
+        .format("delta")\
+        .mode("overwrite")\
+        .save("dbfs:/Prophecy/sparklearner123@gmail.com/RetailVendor/default/PricingReport")
