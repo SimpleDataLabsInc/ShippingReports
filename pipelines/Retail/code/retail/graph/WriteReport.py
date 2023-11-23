123bcdcd5fccd7fb64dff63c6f62f46d48735d5b
anya.bida1@gmail.com <anya.bida1@gmail.com>
2023-11-23 19:25:29 +0000

    🔁 Modified     - Pipeline        - PricingBySegment        - Retail     - Dataset        - Report     - Job        - WeeklyPricingReportPerVendor     - Subgraph        - DataCleanup
    ❇️ New
        - Job
           - RetailJob

--- pipelines/Retail/code/retail/graph/WriteReport.py
+++ pipelines/Retail/code/retail/graph/WriteReport.py
@@ -1,9 +1,10 @@
 from pyspark.sql import *
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
+from prophecy.utils import *
 from prophecy.libs import typed_lit
 from retail.config.ConfigStore import *
 from retail.udfs.UDFs import *
 
 def WriteReport(spark: SparkSession, in0: DataFrame):
-    in0.write.format("parquet").save("dbfs:/Prophecy/sparklearner123@gmail.com/Report")
+    in0.write.format("parquet").save("dbfs:/Prophecy/sparklearner123@gmail.com/CustomersOrdersReport")
